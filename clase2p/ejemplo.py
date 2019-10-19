6d9f9f67701a3bfdd5db379c745903007811642b
jjsanmartin2 <jjsanmartin11182001@hotmail.com>
2019-10-18 23:42:31 -0500

    clase2p

---
+++ clase2p/ejemplo.py
@@ -0,0 +1,17 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Jhandry Javier Sanmartin Jaramillo
+SyntaxError: invalid syntax
+>>> loja
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    loja
+NameError: name 'loja' is not defined
+>>> loja
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    loja
+NameError: name 'loja' is not defined
+>>> bariio la banda
+SyntaxError: invalid syntax
+>>> correo: jjsanmartin11182001@hotmail.com
---
+++ clase2p/ejemplojava2/src/ejemplojava2/Ejemplojava2.java
@@ -0,0 +1,25 @@
+Jhandry Javier Sanmartin Jaramillo
+Loja
+Loja
+Barrio La Banda
+Correo: jjsanmartin11182001@hotmail.com/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplojava2;
+
+/**
+ *
+ * @author Salas
+ */
+public class Ejemplojava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
